eba52a1d2c0a1dad518dc4f3fdabad4f14573cd5
AlekseyImp <73545667+AlekseyImp@users.noreply.github.com>
2020-10-28 22:51:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<?php
+$primer="152*(((())5+2)))()";
+echo $primer;
+$kol_simvolov=strlen($primer);
+echo "<br />";
+$i=0;
+$schet=0;
+
+while($i < $kol_simvolov) {
+	if ($primer[$i] == '(') {
+		$schet++;
+	}
+	if ($primer[$i] == ')') {
+		$schet--;
+	}
+	if ($schet < 0) {
+		echo "Не правильяная последовательность скобок<br />";
+		break;
+	}
+	$i++;
+}
+
+if ($schet > 0) {
+	echo "Количество скобочек не совпало<br />";
+} else if ($schet == 0) {
+	echo "Количество скобочек совпало<br />";
+}
