ccde4a1432b00cac4c720e3b8896c8add87347cc
AlekseyImp <73545667+AlekseyImp@users.noreply.github.com>
2020-10-29 22:35:05 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,40 @@
-п»ї<?php
-$primer="152*(((())5+2)))()";
-echo $primer;
-$kol_simvolov=strlen($primer);
-echo "<br />";
-$i=0;
-$schet=0;
-
-while($i < $kol_simvolov) {
-	if ($primer[$i] == '(') {
-		$schet++;
-	}
-	if ($primer[$i] == ')') {
-		$schet--;
-	}
-	if ($schet < 0) {
-		echo "РќРµ РїСЂР°РІРёР»СЊСЏРЅР°СЏ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚СЊ СЃРєРѕР±РѕРє<br />";
-		break;
-	}
-	$i++;
+<?php
+$str="@gmail.com";
+if(isset($_POST["done"])) {
+if(substr_count($_POST["email"] , $str) >=1) {
+echo "Регистрация пользователя с таким почтовым адресом невозможна. <a href='/'>Исправить</a>";
+return error;
 }
-
-if ($schet > 0) {
-	echo "РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРєРѕР±РѕС‡РµРє РЅРµ СЃРѕРІРїР°Р»Рѕ<br />";
-} else if ($schet == 0) {
-	echo "РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРєРѕР±РѕС‡РµРє СЃРѕРІРїР°Р»Рѕ<br />";
+if($_POST["FIO"] == "") {
+    echo "Введите ФИО. <a href='/'>Исправить</a>";
+    echo "<br />";
 }
+if($_POST["mob"] == "") {
+    echo "Введите номер моб. телефона. <a href='/'>Исправить</a>";
+    echo "<br />";
+}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Форма обратной связи</title>
+</head>
+<body>
+    <form name="feedback" action="" method="post" enctype="multipart/form-data" >
+        <label>Комментарий</label><br />
+        <textarea name="comm" cols="40" rows="10"></textarea><br />
+        <label>ФИО</label><br />
+        <input required="1" type="text" name="FIO" pattern="[а-яА-Я]+" placeholder="ФИО"/><br />
+        <label>Адрес</label><br />
+        <input type="text" name="addres" placeholder="Адрес"/><br />
+        <label>Email</label><br />
+        <input type="text" name="email" placeholder="Email"/><br />
+        <label>Мобильный телефон в формате +71234567890</label><br />
+        <input required="1" type="text" name="mob" pattern="(8|\+7)\d{10}$" placeholder="Моб. телефон"/><br />
+        <label>Файл</label><br />
+        <input type="file" name="file" placeholder="Файл"/><br />
+        <input type="submit" name="done" value="Отправить"/>
+    </form>
+</body>
+</html>
